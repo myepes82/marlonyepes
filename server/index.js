57d6dc8dbd82b29e0f27adccd12899b175bafa9a
Marlon Yepes <myepes82@misena.edu.co>
2023-09-21 18:00:16 -0500

    minimum server app

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+
+const app = express()
+
+
+app.use(express.json({limit: "25mb"}))
+app.use(express.urlencoded({extended: true}))
+app.use(cors({
+    allowedHeaders: "*",
+    origin: "*"
+}))
+app.use(morgan("common"))
+
+app.get("/api/healt", (req, res) => {
+    res.send({
+        "status": "ok"
+    }).status(200)
+})
+
+
+app.listen(8080, () => console.log("server running at port 8080"))
